01716729073c41108c097a852d53c5afd15fdf76
Idyl Ícaro dos Santos <idylicaro.se@hotmail.com>
2022-01-15 10:21:46 -0300

    Update settings.py

--- todo_app/settings.py
+++ todo_app/settings.py
@@ -84,6 +84,9 @@ DATABASES = {
     }
 }
 
+# Update database configuration with $DATABASE_URL.
+db_from_env = dj_database_url.config(conn_max_age=500)
+DATABASES['default'].update(db_from_env)
 
 # Password validation
 # https://docs.djangoproject.com/en/4.0/ref/settings/#auth-password-validators
