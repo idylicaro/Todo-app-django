b81ea69ce549ca35737909e41e79d45411e4cf11
Idyl Ícaro dos Santos <idylicaro.se@hotmail.com>
2022-01-15 10:28:06 -0300

    add STATIC_ROOT config in settings.py

--- todo_app/settings.py
+++ todo_app/settings.py
@@ -124,7 +124,8 @@ LOGIN_URL = 'login'
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.0/howto/static-files/
 
-STATIC_URL = 'static/'
+STATIC_ROOT = os.path.join(PROJECT_ROOT, 'staticfiles')
+STATIC_URL = '/static/'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
